838cf7075126a7a741b4f8613f43842c5d561a5b
Huseyin Haluk Nurbaki <huseyin.nurbaki@eteration.com>
2020-05-31 03:32:10 +0300

    icons

--- client/src/components/MockItem/index.js
+++ client/src/components/MockItem/index.js
@@ -2,42 +2,41 @@ import React from "react"
 import InputGroup from "react-bootstrap/InputGroup"
 import FormControl from "react-bootstrap/FormControl"
 import Button from "react-bootstrap/Button"
+import { Row, Col, Badge } from "react-bootstrap"
 
 const MockItem = (props) => {
-  // let method = props.data.method.toString().toUpperCase()
-  let bgc = "orange"
-  // if (method === "GET") {
-  //   bgc = "#17b027"
-  //   method = method + "  ."
-  // }
-  let path = '/' + props.data.path
-  let copypath = "http://localhost:7080/mocktail/" + props.data.path
+  let originalname = props.data.originalname
+  const createDate = props.data.createDate
+    ? new Date(parseInt(props.data.createDate)).toDateString()
+    : null
+  const createTime = props.data.createDate
+    ? new Date(parseInt(props.data.createDate)).toTimeString()
+    : null
   return (
     <dt key={props.index}>
       <InputGroup className="mb-3">
         <InputGroup.Prepend>
-          <InputGroup.Text
-            style={{ backgroundColor: bgc, color: "white", fontWeight: "600" }}
-            id="basic-addon1"
-          >
-            {props.originalname}
-          </InputGroup.Text>
+          <img src="ipa.png" style={{ height: "80px", opacity: "0.85" }} alt="" />
         </InputGroup.Prepend>
-        <FormControl
-          placeholder={path}
-          aria-label={path}
-          aria-describedby="basic-addon1"
-          disabled
-        />
+        <Col>
+          <Row>
+            <FormControl
+              placeholder={originalname + " " + createDate}
+              aria-label={originalname}
+              aria-describedby="basic-addon1"
+              disabled
+            />
+          </Row>
+          <Row>
+            <FormControl
+              placeholder={createTime}
+              aria-label={createTime}
+              aria-describedby="basic-addon1"
+              disabled
+            />
+          </Row>
+        </Col>
         <InputGroup.Append>
-          <Button
-            onClick={() => {
-              navigator.clipboard.writeText(copypath)
-            }}
-            variant="outline-secondary"
-          >
-            Copy
-          </Button>
           {!props.disabled ? (
             <Button
               onClick={
@@ -50,6 +49,16 @@ const MockItem = (props) => {
           ) : null}
         </InputGroup.Append>
       </InputGroup>
+      <Col>
+        <Row>
+          <div>
+            <h4>Tags : </h4>
+          </div>
+          <div>
+            <Badge variant="primary">Primary</Badge>{" "}
+          </div>
+        </Row>
+      </Col>
     </dt>
   )
 }
